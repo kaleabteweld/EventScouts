756c99e7007f65ba1866353326a019fa31164313
kaleab Teweldbrhan Abay <42609047+kaleabteweld@users.noreply.github.com>
2024-03-13 11:27:33 +0300

    added jsDoc for Transaction

--- api/src/Domains/Transaction/router.ts
+++ api/src/Domains/Transaction/router.ts
@@ -6,6 +6,40 @@ import TransactionController from "./controller";
 const publicTransactionRouter = express.Router();
 const privateTransactionRouter = express.Router();
 
+/**
+ * @swagger
+ * /private/transaction/mint:
+ *   patch:
+ *     summary: Mint transaction
+ *     tags: [Transaction]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/newTransactionJsdocSchema'
+ *     responses:
+ *       200:
+ *         description: Mint transaction successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Transaction'
+ *       400:
+ *         description: Error occurred
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/validationError'      
+ *       401:
+ *         description: No Valid Token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/NoValidToken'
+ */
 privateTransactionRouter.patch("/mint", userOnly, MakeErrorHandler(
     async (req: any, res: Response) => {
         res.json(await TransactionController.mint(req.body, req['user']));
